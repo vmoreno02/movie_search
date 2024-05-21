0e8e41fffa5826ebae3c35bb0f4e6b1d52b93d2c
Victoria Moreno <vkeimoreno@gmail.com>
2024-05-21 16:11:04 -0600

    Added /movies/:title endpoint and corresponding database logic.

---
+++ webservice/database.js
@@ -0,0 +1,31 @@
+// Accesses the Movie Database 
+// If more functionality and different API calls were added in the future, they could be placed here
+
+const fetch = require('node-fetch');
+
+// authorization
+// fix later (env variables?)
+const API_KEY = "eyJhbGciOiJIUzI1NiJ9"
+const API_READ_ACCESS_TOKEN = "eyJhdWQiOiIzMmNkYTA5NzA5NDZhY2FkNDljZjkzMTVhOWQ4ZDA2OSIsInN1YiI6IjY2NGQwOTk4YzliZWU0MzY1YTAxYTQ1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.at73PEzjL_3-6Kocv0BogTIGWU8pMpGqgFrTRvp2y4k"
+
+async function searchMovies(query) {
+    const url = 'https://api.themoviedb.org/3/search/movie?query=' + query + '&include_adult=false&language=en-US&page=1';
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ' + API_KEY + '.' + API_READ_ACCESS_TOKEN
+    }
+    };
+
+    return fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+}
+
+async function test() {
+    const res = await searchMovies("the avengers");
+}
+
+test();
--- webservice/index.js
+++ webservice/index.js
@@ -1,10 +1,51 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3002;
 
-app.get('/', async (req, res) => {
-    res.send('Hello, World!');
+// database access
+const db = require("./database.js")
+
+// GET /movies/:title
+// Searches database for matching titles and returns the first 10 results (if applicable)
+app.get('/movies/:title', async (req, res) => {
+    try {
+        const movieResult = await db.searchMovies(req.params.title);
+        let movieList = movieResult.results;
+
+        if (movieResult.total_results > 10) {
+            movieList = movieList.slice(0, 10);
+        }
+
+        const editedList = editMovieList(movieList);
+        
+        res.json(editedList);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+
+// helper function to clean up movie list
+async function editMovieList(movieList) {
+    let editedList = [];
+
+    for (i = 0; i < movieList.length; i++) {
+        const popularity = movieList[i].popularity;
+        const vote_count = movieList[i].vote_count;
+
+        let listItem = {
+            movie_id: movieList[i].id,
+            title: movieList[i].original_title,
+            poster_image_url: "https://image.tmdb.org/t/p/original/" + movieList[i].poster_path,
+            popularity_summary: popularity + " out of " + vote_count
+        };
+
+        editedList.push(listItem);
+    }
+
+    return editedList;
+}
 
 // start the app
 app.listen(PORT, () => {
